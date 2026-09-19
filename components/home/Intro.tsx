f9465b131c63afb648473a9b67fb0ee6553238c4
Mohammad Noaeen <82122012+noaeen@users.noreply.github.com>
2026-09-19 22:25:34 +0000

    Refactor SVG assets: Replace arc-icon.png with arc-icon.svg, delete old arc-logo.svg, and add new arc-text.svg

--- components/home/Intro.tsx
+++ components/home/Intro.tsx
@@ -7,7 +7,7 @@ export default function SliderControlsInside() {
                 <div className="bg-white rounded-xl p-8 shadow-sm flex items-center justify-center gap-20">
                     
                     <div className="w-1/3 flex justify-center">
-                        <img src="/arc-icon.png" className="w-80" />
+                        <img src="/arc-icon.svg" className="w-80" />
                     </div>
 
                     <div className="w-2/3 max-w-3xl space-y-3">
